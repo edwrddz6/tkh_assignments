477b2a250a52ae95d682cc10067e868b4990d2d8
edwrddz6 <134966646+edwrddz6@users.noreply.github.com>
2023-10-16 22:18:22 -0700

    Add javascript code

---
+++ exercise_javascript_3/script.js
@@ -0,0 +1,67 @@
+function GiveMeResult() {
+    let firstOperand = Number(document.getElementById("firstOperand").value);
+  
+    let operator = document.getElementById("operators").value;
+  
+    let secondOperand = Number(document.getElementById("secondOperand").value);
+  
+    let resultSpan = document.getElementById("result");
+  
+    let resultNum = null;
+  
+    switch (operator) {
+      case '+':
+        resultNum = firstOperand + secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '-':
+        resultNum = firstOperand - secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '*':
+        resultNum = firstOperand * secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '/':
+        resultNum = firstOperand / secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '%':
+        resultNum = firstOperand % secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '**':
+        resultNum = firstOperand ** secondOperand;
+        resultSpan.innerHTML = resultNum
+        break;
+      case '>':
+        if(firstOperand > secondOperand){
+          resultSpan.innerHTML = "true"
+        } else {
+          resultSpan.innerHTML = "false";
+        }
+        break;
+      case '<':
+        if(firstOperand < secondOperand) {
+            resultSpan.innerHTML = "true"
+        } else {
+            resultSpan.innerHTML = "false";
+        }
+        break;
+      case '!=':
+        if(firstOperand != secondOperand) {
+        resultSpan.innerHTML = "true"
+        } else {
+        resultSpan.innerHTML = "false";
+        }
+        break;
+      default:
+        console.log('No Operator Found!');
+    }
+  }
+  
+  let button = document.getElementById("button");
+  
+  button.addEventListener('click', GiveMeResult);
+  
+  
