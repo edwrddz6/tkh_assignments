9eb013a87124b605dde8026d8e8924b212d9747a
edwrddz6 <134966646+edwrddz6@users.noreply.github.com>
2023-10-18 21:43:41 -0700

    Add more conditional statements to exercise2

--- assigned_reading_2/script.js
+++ assigned_reading_2/script.js
@@ -10,10 +10,20 @@ for (let number = '#'; number.length <= 7; number = number + '#') {
 // Exercise #2 : FizzBuzz
 
 for (let number = 0; number <= 100; number = number + 1) {
-  if (number % 3 != 0) {
-    console.log(number)
+  if (number % 3 != 0 && number % 5 != 0) {
+    console.log(number);
   }
-  if (number % 3 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log('FizzBuzz');
+  }
+  else if (number % 3 === 0) {
     console.log('Fizz');
   }
+  else if (number % 5 === 0) {
+    console.log('Buzz');
+  }
 }
+
+
+
+
